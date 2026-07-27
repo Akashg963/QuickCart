643f63b8225ea6bbdaf7e77593ec5431cb977829
Akash963 <akashgkp124k@gmail.com>
2026-07-27 12:54:19 +0530

    First Commit

---
+++ config/inngest.js
@@ -0,0 +1,53 @@
+// src/inngest/client.ts
+import { Inngest } from "inngest";
+
+export const inngest = new Inngest({ id: "quickcart-next" });
+
+export const syncUserCart = inngest.createFunction(
+    {
+        id: "sync-user-from-clerk"
+    },
+    {event: "clerk/user.created"},
+    async ({ event}) => {
+        const { id, email_addresses, first_name, last_name, profile_image_url } = event.data;
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            imageUrl: profile_image_url,
+            name: `${first_name} ${last_name}`,
+        };
+        await dbConnect();
+        await User.create(userData);
+    }
+)
+
+export const syncUserUpdation = inngest.createFunction(
+    {
+        id: "update-user-from-clerk"
+    },
+    {event: "clerk/user.updated"},
+    async ({ event}) => {
+        const { id, email_addresses, first_name, last_name, profile_image_url } = event.data;
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            imageUrl: profile_image_url,
+            name: `${first_name} ${last_name}`,
+        };
+        await dbConnect();
+        await User.findByIdAndUpdate(id, userData);
+    }
+)
+
+
+export const syncUserDeletion = inngest.createFunction(
+    {
+        id: "delete-user-with-clerk"
+    },
+    {event: "clerk/user.deleted"},
+    async ({ event}) => {
+        const { id } = event.data;
+        await dbConnect();
+        await User.findByIdAndDelete(id);
+    }
+)
---
+++ models/User.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    cartItems: {
+        type: Object,
+        of: Number,
+        default: {}
+    }
+}, {minimize: false});
+
+const User = mongoose.models.User || mongoose.model.User ("User", userSchema);
+
+export default User;
