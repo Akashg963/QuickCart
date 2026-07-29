93bce938f5d63131a4e7c4a2b17e38bc485f24f2
Akash963 <akashgkp124k@gmail.com>
2026-07-29 08:41:40 +0530

    Fix Inngest imports

--- config/inngest.js
+++ config/inngest.js
@@ -1,5 +1,7 @@
 // src/inngest/client.ts
 import { Inngest } from "inngest";
+import dbConnect from "@/config/db";
+import User from "@/models/User";
 
 export const inngest = new Inngest({ id: "quickcart-next" });
 
