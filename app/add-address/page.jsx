24f99f9c82dbbeef23e085e7abc910a6a1360003
Akash963 <akashgkp124k@gmail.com>
2026-08-14 09:06:44 +0530

    Fix add-address page useAppContext error

--- app/add-address/page.jsx
+++ app/add-address/page.jsx
@@ -1,4 +1,6 @@
 'use client'
+import React from "react";
+import { useAppContext } from "@/context/AppContext";
 import { assets } from "@/assets/assets";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
